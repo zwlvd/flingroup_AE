0be44004c78975da0dcfc2be4023de606c301241
akuza <1277784810@qq.com>
2024-11-20 12:05:55 +0800

    init

---
+++ ae_vue/src/api/main/case.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询案例库列表
+export function listCase(query) {
+  return request({
+    url: '/main/case/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询案例库详细
+export function getCase(caseId) {
+  return request({
+    url: '/main/case/' + caseId,
+    method: 'get'
+  })
+}
+
+// 新增案例库
+export function addCase(data) {
+  return request({
+    url: '/main/case',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改案例库
+export function updateCase(data) {
+  return request({
+    url: '/main/case',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除案例库
+export function delCase(caseId) {
+  return request({
+    url: '/main/case/' + caseId,
+    method: 'delete'
+  })
+}
